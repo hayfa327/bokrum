bdc1a5a23cd0fb9478fa27b9b8d2ff72c641594d
hayfa safa <haifasafaa@gmail.com>
2026-08-19 20:01:48 +0200

    Fix .gitignore for db files, add Identity/EF Core setup to Program.cs, add initial migration

--- backend/BookQuotesApi/Program.cs
+++ backend/BookQuotesApi/Program.cs
@@ -1,5 +1,25 @@
+using BookQuotesApi.Data;
+using BookQuotesApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// --- Database (SQLite) ---
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// --- Identity ---
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.User.RequireUniqueEmail = true;
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// --- Controllers, Swagger ---
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,6 +33,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
